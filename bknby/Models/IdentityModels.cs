44b99920acc76d32434dd7816cc14564e886c60e
Kiryl Aliokhin <pet_ros86@list.ru>
2017-11-18 22:31:47 +0300

    tuning connection string

--- bknby/Models/IdentityModels.cs
+++ bknby/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace bknby.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("bknProd", throwIfV1Schema: false)
         {
         }
 
