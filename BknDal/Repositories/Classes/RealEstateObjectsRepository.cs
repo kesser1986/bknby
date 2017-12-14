256a77a5f4ded60b695639bb9231ef5f07c5414b
Kiryl Aliokhin <pet_ros86@list.ru>
2017-12-15 00:13:08 +0300

    Revert "Revert "Added IoC container""
    This reverts commit ac458d702001e36709384ec90bc71d4ed6fd401a.

--- BknDal/Repositories/Classes/RealEstateObjectsRepository.cs
+++ BknDal/Repositories/Classes/RealEstateObjectsRepository.cs
@@ -1,9 +1,18 @@
-﻿using BknDal.Models;
+﻿using System;
+using System.Threading.Tasks;
+using BknDal.Models;
 using BknDal.Repositories.Interfaces;
 
 namespace BknDal.Repositories.Classes
 {
     public class RealEstateObjectsRepository : BaseRepository<RealEstateObject, long>, IRealEstateObjectsRepository
     {
+        public async Task ImportRealEstateObjects()
+        {
+            using (var db = DbContextFactory.OpenContext())
+            {
+                
+            }
+        }
     }
 }
--- bknby/Controllers/RealEstateObjectsController.cs
+++ bknby/Controllers/RealEstateObjectsController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BknDal.Repositories.Classes;
+using BknDal.Repositories.Interfaces;
+using BknService.Services.Classes;
+using BknService.Services.Interfaces;
+using Ninject;
 
 namespace bknby.Controllers
 {
     public class RealEstateObjectsController : Controller
     {
+        private readonly IRealEstateObjectsService _realEstateObjectsService;
+
+        public RealEstateObjectsController(IRealEstateObjectsService realEstateObjectsService)
+        {
+            _realEstateObjectsService = realEstateObjectsService;
+        }
+
         [Authorize]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [Authorize]
+        public ActionResult ImportRealEstateObjects()
+        {
+
+            _realEstateObjectsService.ImportRealEstateObjects();
+
+            return View("Index");
         }
 
         [Authorize]
